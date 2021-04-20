40f22ab2cf035d1a71034be2b4dce9ac578eda01
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-04-21 02:19:50 +0800

    codigos y decodigos

--- Midterm/Decode 2 days of future past.cpp
+++ Midterm/Decode 2 days of future past.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <bits/stdc++.h>
 #include <string>
 using namespace std;
 
@@ -7,25 +6,19 @@ class Decode
 {
 public:
     string decodigo;
-    int len;
 
     Decode()
     {
         decodigo="";
-        len=decodigo.size();
     }
     Decode(string word)
     {
         for(int i=0;i<word.size();i+=3)
         {
-            int a=0;
-            for(int j=0;j<3;++j)
-            {
-                a+=((int)word[i-j]-48)*pow(10,j);
-            }
-            decodigo.insert(0,1,(char)a);
+            char partei[3];
+            word.copy(partei,3,i);
+            decodigo+=(char)stoi(partei);
         }
-        len=decodigo.size();
     }
 
     string decode()
